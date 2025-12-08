74021bd55d0fcdaeade7488fe19435c3f2d5e852
Emory Merryman <emory.merryman@gmail.com>
2025-12-07 19:19:22 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
                                                                                             name = "release-application" ;
                                                                                             extraBwrapArgs =
                                                                                                 [
-                                                                                                    "--ro-bind ${ mount-directory }/$INDEX /mount"
+                                                                                                    "--ro-bind ${ mounts-directory }/$INDEX /mount"
                                                                                                     "--tmpfs /scratch"
                                                                                                 ] ;
                                                                                             runScript =
