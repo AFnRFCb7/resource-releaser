60dd6e2205c72ad507745f1c6d0c1d46121e6685
Emory Merryman <emory.merryman@gmail.com>
2026-02-05 14:10:49 -0500

    up

--- flake.nix
+++ flake.nix
@@ -186,7 +186,7 @@
                                                                                             tar --create --file "$TEMPORARY" --xz "${ locks-directory }/$INDEX" "${ mounts-directory }/$INDEX" "${ quarantine-directory }/$INDEX" "${ gc-roots-directory }/$INDEX"
                                                                                             rm --recursive --force "${ locks-directory }/$INDEX" "${ mounts-directory }/$INDEX" "${ quarantine-directory }/$INDEX" "${ gc-roots-directory }/$INDEX"
                                                                                             nix-collect-garbage
-                                                                                           JSON="$( jq --null-input --compact-output --arg HASH "$HASH" { "hash" : $HASH , type : "RELEASE_SUCCESS" } )" || failure 215bca0e
+                                                                                           JSON="$( jq --null-input --compact-output --arg HASH "$HASH" '{ "hash" : $HASH , type : "RELEASE_SUCCESS" }' )" || failure 215bca0e
                                                                                            redis-cli PUBLISH ${ channel } "$JSON"
                                                                                        else
                                                                                            mkdir --parents "${ quarantine-directory }/$INDEX/release"
@@ -230,7 +230,7 @@
                                                                                                envsubst < ${ resolve } > "${ quarantine-directory }/$INDEX/release/$RESOLUTION"
                                                                                                chmod 0500 "${ quarantine-directory }/$INDEX/release/$RESOLUTION"
                                                                                            done
-                                                                                           JSON="$( jq --null-input --compact-output --arg HASH "$HASH" { "hash" : $HASH , type : "RELEASE_FAILURE" } )" || failure e979e6dd
+                                                                                           JSON="$( jq --null-input --compact-output --arg HASH "$HASH" '{ "hash" : $HASH , type : "RELEASE_FAILURE" }' )" || failure e979e6dd
                                                                                            redis-cli PUBLISH ${ channel } "$JSON"
                                                                                        fi
                                                                                        rm "$STANDARD_OUTPUT_FILE" "$STANDARD_ERROR_FILE"
