1daed9b8a39c0f003e0d632f33169ecacfc4243a
Emory Merryman <emory.merryman@gmail.com>
2026-03-05 12:56:34 -0500

    abc

--- flake.nix
+++ flake.nix
@@ -85,15 +85,12 @@
                                                                                             ;;
                                                                                     esac
                                                                                 done
-                                                                                echo 7e1212fd e6f55c16 >> /tmp/DEBUG
                                                                                 rm "${ resources-directory }/marks/$INDEX"
-                                                                                echo 7e1212fd ec45f207 >> /tmp/DEBUG
                                                                                 find "${ resources-directory }/originator-pids/$INDEX" -mindepth 1 -maxdepth 1 -type f | while read -r PID_FILE
                                                                                 do
                                                                                     PID="$( basename "$PID_FILE" )" || failure 6142318a
                                                                                     tail --follow /dev/null --pid "$PID"
                                                                                 done
-                                                                                echo 7e1212fd 6bf14776 >> /tmp/DEBUG
                                                                                 find ${ root-directory } -type l | while read -r LINK
                                                                                 do
                                                                                     RESOURCE="$( readlink --canonicalize "$LINK" )" || failure 64949f94
@@ -102,19 +99,14 @@
                                                                                         inotify-wait --event delete-self "$LINK" || true
                                                                                     fi
                                                                                 done
-                                                                                echo 7e1212fd 65d2c5a2 >> /tmp/DEBUG
                                                                                 exec 203> "${ resources-directory }/locks/$HASH"
                                                                                 flock -x 203
-                                                                                echo 7e1212fd 7050989e >> /tmp/DEBUG
                                                                                 if [[ -f ${ resources-directory }/marks/$INDEX ]]
                                                                                 then
-                                                                                    echo 7e1212fd b3c4854e >> /tmp/DEBUG
                                                                                     nohup "$ITERATION" --hash "$HASH" --index "$INDEX" --release "$RELEASE" &
                                                                                 else
-                                                                                    echo 7e1212fd c3c16c3d >> /tmp/DEBUG
                                                                                     STANDARD_ERROR_FILE="$( mktemp )" || failure 479f37a
                                                                                     STANDARD_OUTPUT_FILE="$( mktemp )" || failure 9273f3b8
-                                                                                    echo 7e1212fd 84201603 >> /tmp/DEBUG
                                                                                     export APPLICATION="$RELEASE/bin/release"
                                                                                     if release-application > "$STANDARD_OUTPUT_FILE" 2> "$STANDARD_ERROR_FILE"
                                                                                     then
@@ -124,7 +116,6 @@
                                                                                     fi
                                                                                     STANDARD_ERROR="$( cat "$STANDARD_ERROR_FILE" )" || failure dd6c09a4
                                                                                     STANDARD_OUTPUT="$( cat "$STANDARD_OUTPUT_FILE" )" || failure d3e55660
-                                                                                    echo 7e1212fd 8fbd9d3a "STATUS=$STATUS" "STANDARD_ERROR=$STANDARD_ERROR" /tmp/DEBUG
                                                                                     if [[ "$STATUS" == 0 ]] && [[ ! -s "$STANDARD_ERROR_FILE" ]]
                                                                                     then
                                                                                         echo 7e1212fd eed5220f
